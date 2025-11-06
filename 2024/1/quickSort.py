5c76d5dde025dc87e05ef5dadb532b9acf49d965
Krzysztof Kokot <krzysiukokot@interia.pl>
2025-11-06 17:39:58 +0100

    refactoring 2024 Task 1

--- 2024/1/quickSort.py
+++
@@ -1,7 +0,0 @@
-def quickSort(list):
-    if len(list) <= 1:
-        return list
-    pivot = list[0]
-    left = [x for x in list[1:] if x < pivot]
-    right = [x for x in list[1:] if x >= pivot]
-    return quickSort(left) + [pivot] + quickSort(right)
