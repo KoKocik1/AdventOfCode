c861ee97ba4091a60523f26e47ab89a9718c8c46
Krzysztof Kokot <krzysiukokot@interia.pl>
2025-11-11 08:38:06 +0100

    Task 6 2024 Part 1

---
+++ 2024/8/classes.py
@@ -0,0 +1,40 @@
+from helpers.array_helper import Board, Position
+
+
+class AntennaAnalyzer:
+    def __init__(self, board: Board):
+        self.board = board
+
+    def get_mirror_point(self, main_point: Position, secondary_point: Position) -> Position:
+        new_row = main_point.row - secondary_point.row
+        new_col = main_point.col - secondary_point.col
+        return Position(main_point.row + new_row, main_point.col + new_col)
+
+    def _unique_signal_characters(self) -> set[str]:
+        # Build a set of unique non-empty signal characters (exclude '.')
+        unique_chars = self.board.get_all_characters()
+        unique_chars.remove('.')
+        return unique_chars
+
+    def find_antinodes(self) -> set[Position]:
+        """
+        For each signal character, for every ordered pair of distinct antenna positions
+        (A, B), add the mirror of B through A: M = A + (A - B), if it lies on the board.
+        """
+        antinodes: set[Position] = set()
+        for character in self._unique_signal_characters():
+            positions = self.board.find_all_character_positions(character)
+            if len(positions) < 2:
+                continue
+            for main_point in positions:
+                for other in positions:
+                    if other == main_point:
+                        continue
+                    mirror_point = self.get_mirror_point(main_point, other)
+                    if self.board.is_out_of_board(mirror_point):
+                        continue
+                    antinodes.add(mirror_point)
+        return antinodes
+
+    def count_antinodes(self) -> int:
+        return len(self.find_antinodes())
