19dccaaaddd6795ada8febc44dbe8302d0ba0a14
Krzysztof Kokot <krzysiukokot@interia.pl>
2025-11-12 15:57:40 +0100

    Task 9 2024 Part 2

--- 2024/9/classes.py
+++ 2024/9/classes.py
@@ -1,6 +1,33 @@
 from helpers import GetFile
 
 
+class File:
+    def __init__(self):
+        self.file_ID = -1
+        self.file_size = 0
+        self.position = 0
+
+    def set_file_ID(self, file_ID: int):
+        self.file_ID = file_ID
+
+    def set_position(self, position: int):
+        self.position = position
+        self.file_size += 1
+
+
+class EmptySpace:
+    def __init__(self):
+        self.size = 0
+        self.first_position = 0
+
+    def add_empty_space(self, first_position: int):
+        if self.size == 0:
+            self.first_position = first_position
+            self.size = 1
+        else:
+            self.size += 1
+
+
 class DiscSpace:
     def __init__(self):
         self.disc_space = []
@@ -8,6 +35,7 @@ class DiscSpace:
     def load_disc_space(self, file: GetFile):
         is_file = True
         file_ID = 0
+        empty_ID = -1
         for row in file.get_row():
             for char in row:
                 char_to_int = int(char)
@@ -16,6 +44,7 @@ class DiscSpace:
                         self.disc_space.append(file_ID)
                     else:
                         self.disc_space.append(-1)
+
                 if is_file:
                     file_ID += 1
                 is_file = not is_file
@@ -30,6 +59,29 @@ class DiscSpace:
         for i in range(act_position, -1, -1):
             if self.disc_space[i] != -1:
                 return i
+        return None
+
+    def _find_first_empty_space_for_file(self, file: File) -> EmptySpace:
+        empty_space = EmptySpace()
+        for i in range(0, file.position):
+            if self.disc_space[i] == -1:
+                empty_space.add_empty_space(i)
+                if empty_space.size == file.file_size:
+                    return empty_space
+            else:
+                empty_space.size = 0
+        return None
+
+    def _find_last_file_position(self, act_position: int, last_file_IDs: set[int]) -> File:
+        found_file = False
+        file = File()
+        for i in range(act_position, -1, -1):
+            if self.disc_space[i] not in last_file_IDs and self.disc_space[i] != -1 and (not found_file or self.disc_space[i] == file.file_ID):
+                file.set_file_ID(self.disc_space[i])
+                file.set_position(i)
+                found_file = True
+            elif found_file:
+                return file
         return None
 
     def remove_empty_slots(self):
@@ -50,6 +102,28 @@ class DiscSpace:
                 empty_position + 1)
             number_position = self._find_last_number_position(
                 number_position - 1)
+
+    def clear_disc_space_all_files(self):
+        last_file_IDs = set()
+        file_to_move = self._find_last_file_position(
+            len(self.disc_space) - 1, last_file_IDs)
+
+        empty_position = self._find_first_empty_space_for_file(file_to_move)
+
+        while file_to_move is not None or file_to_move.position <= file_to_move.file_size:
+            if empty_position is not None:
+                difference = empty_position.first_position - file_to_move.position
+                # move the block from number_position to the first empty slot
+                for i in range(file_to_move.position, file_to_move.position + file_to_move.file_size):
+                    self.disc_space[i + difference] = self.disc_space[i]
+                    last_file_IDs.add(self.disc_space[i])
+                    self.disc_space[i] = -1
+            file_to_move = self._find_last_file_position(
+                file_to_move.position - 1, last_file_IDs)
+            if file_to_move is None or file_to_move.file_ID == 0:
+                break
+            empty_position = self._find_first_empty_space_for_file(
+                file_to_move)
 
     def calculate_checksum(self) -> int:
         checksum = 0
--- 2024/9/main.py
+++ 2024/9/main.py
@@ -11,10 +11,18 @@ def part1(file: GetFile) -> int:
     return disc_space.calculate_checksum()
 
 
+def part2(file: GetFile) -> int:
+    disc_space = DiscSpace()
+    disc_space.load_disc_space(file)
+    disc_space.clear_disc_space_all_files()
+    return disc_space.calculate_checksum()
+
+
 def main():
     data_file = Path(__file__).parent / 'data/data.txt'
     file = GetFile(str(data_file), delimiter='')
     print(part1(file))
+    print(part2(file))
 
 
 if __name__ == "__main__":
