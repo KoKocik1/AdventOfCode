18f20a9ce1eb26d36a7409bffe12ef4520a3c8c5
Krzysztof Kokot <krzysiukokot@interia.pl>
2025-11-13 08:12:59 +0100

    Array helper Refactoring

--- 2024/helpers/array_helper.py
+++ 2024/helpers/array_helper.py
@@ -1,31 +1,26 @@
 
 class Position:
-    row: int
-    col: int
-
-    def __init__(self, row, col):
+    def __init__(self, row: int, col: int):
         self.row = row
         self.col = col
 
     @staticmethod
-    def create_from_position(position: 'Position'):
+    def create_from_position(position: 'Position') -> 'Position':
         return Position(position.row, position.col)
 
-    def __eq__(self, other):
+    def __eq__(self, other: object) -> bool:
         """Compare two Position objects by their row and col values."""
         if not isinstance(other, Position):
             return False
         return self.row == other.row and self.col == other.col
 
-    def __hash__(self):
+    def __hash__(self) -> int:
         """Make Position hashable so it can be used in sets and as dictionary keys."""
         return hash((self.row, self.col))
 
 
 class Board:
-    board: list[list[str]]
-
-    def __init__(self, board):
+    def __init__(self, board: list[list[str]]):
         self.board = board
 
     def get_row_length(self) -> int:
@@ -40,7 +35,7 @@ class Board:
     def is_character_at_position(self, position: Position, character: str) -> bool:
         return self.get_character(position) == character
 
-    def find_character_position(self, character: str) -> Position:
+    def find_character_position(self, character: str) -> Position | None:
         for row in range(self.get_column_length()):
             for col in range(self.get_row_length()):
                 if self.is_character_at_position(Position(row, col), character):
@@ -56,7 +51,8 @@ class Board:
         return positions
 
     def get_all_characters(self) -> list[str]:
-        characters = set()
+        """Return a list of all unique characters on the board."""
+        characters: set[str] = set()
         for row in range(self.get_column_length()):
             for col in range(self.get_row_length()):
                 characters.add(self.get_character(Position(row, col)))
@@ -66,7 +62,9 @@ class Board:
         self.board[position.row][position.col] = character
 
     def is_out_of_board(self, position: Position) -> bool:
-        return position.row < 0 or position.row >= self.get_column_length() or position.col < 0 or position.col >= self.get_row_length()
+        """Check if a position is outside the board boundaries."""
+        return (position.row < 0 or position.row >= self.get_column_length() or
+                position.col < 0 or position.col >= self.get_row_length())
 
     def count_characters(self, character: str) -> int:
         count = 0
@@ -83,30 +81,46 @@ class Board:
         return self.__str__()
 
 
+class Directions:
+    UP = 'up'
+    RIGHT = 'right'
+    DOWN = 'down'
+    LEFT = 'left'
+
+
 class Player:
     def __init__(self, board: Board):
         self.board = board
-        self.directions = ['up', 'right', 'down', 'left']
+        self.directions = [Directions.UP, Directions.RIGHT,
+                           Directions.DOWN, Directions.LEFT]
         self.direction = 0
 
     def turn_right(self) -> None:
         self.direction = (self.direction + 1) % 4
 
-    def move(self, act_position: Position) -> Position:
-        new_position = None
-        if self.directions[self.direction] == 'up':
-            new_position = Position(act_position.row - 1, act_position.col)
-        elif self.directions[self.direction] == 'right':
-            new_position = Position(act_position.row, act_position.col + 1)
-        elif self.directions[self.direction] == 'down':
-            new_position = Position(act_position.row + 1, act_position.col)
-        elif self.directions[self.direction] == 'left':
-            new_position = Position(act_position.row, act_position.col - 1)
+    def move(self, current_position: Position) -> Position | None:
+        """Move one step in the current direction from the given position."""
+        direction_name = self.directions[self.direction]
+
+        if direction_name == Directions.UP:
+            new_position = Position(
+                current_position.row - 1, current_position.col)
+        elif direction_name == Directions.RIGHT:
+            new_position = Position(
+                current_position.row, current_position.col + 1)
+        elif direction_name == Directions.DOWN:
+            new_position = Position(
+                current_position.row + 1, current_position.col)
+        elif direction_name == Directions.LEFT:
+            new_position = Position(
+                current_position.row, current_position.col - 1)
+        else:
+            return None
+
         if self.board.is_out_of_board(new_position):
             return None
         return new_position
 
     def is_valid_position(self, position: Position, character: str) -> bool:
-        if self.board.is_character_at_position(position, character):
-            return False
-        return True
+        """Check if position does NOT contain the specified character."""
+        return not self.board.is_character_at_position(position, character)
