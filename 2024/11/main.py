2b757350c6f81b65f14dda0339db6b3605fa4438
Krzysztof Kokot <krzysiukokot@interia.pl>
2025-11-14 20:33:29 +0100

    Task 11 2024 Part 1&2

--- 2024/11/main.py
+++ 2024/11/main.py
@@ -1,26 +1,29 @@
 from pathlib import Path
+from tqdm import tqdm
 
 from helpers import GetFile
-from classes import Stones
+from classes import Stones, RecursiveStones
 
 
 def part1(rows: list[str], blinks: int) -> int:
-    stones = Stones.create_from_string(rows)
+    stones = Stones(rows)
     for _ in range(blinks):
         stones.blink()
-        # print(stones)
     return stones.number_of_stones()
 
 
-def part2(rows: list[list[int]]) -> int:
-    return 0
+def part2(rows: list[str], blinks: int) -> int:
+    stones = RecursiveStones(rows)
+    return stones.recursive_blink(blinks)
 
 
 def main():
     data_file = Path(__file__).parent / 'data/data.txt'
     file = GetFile(str(data_file), delimiter=' ')
+
     for row in file.get_row():
         print(part1(row, 25))
+        print(part2(row, 75))
 
 
 if __name__ == "__main__":
