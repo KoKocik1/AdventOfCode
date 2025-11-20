eb2f85e3cf9a7d1e3013e46719099d0754f52a20
Krzysztof Kokot <krzysiukokot@interia.pl>
2025-11-20 11:54:41 +0100

    Refactoring Task 14 2024 Part 1&2

--- 2024/14/main.py
+++ 2024/14/main.py
@@ -1,40 +1,64 @@
 from pathlib import Path
 import math
+from copy import deepcopy
 
 from helpers import GetFile
 from classes import Board, Robot
 
 
-def load_data(file: GetFile) -> Board:
-    board = Board(101, 103)
+# Constants
+BOARD_WIDTH = 101
+BOARD_HEIGHT = 103
+PART1_STEPS = 100
+
+
+def parse_robot_data(line: list[str]) -> tuple[tuple[int, int], tuple[int, int]]:
+    """Parse robot position and velocity from a line of input."""
+    position_str = line[0].split('=')[1]
+    velocity_str = line[1].split('=')[1]
+
+    position = tuple(int(x) for x in position_str.split(','))
+    velocity = tuple(int(x) for x in velocity_str.split(','))
+
+    return position, velocity
+
+
+def load_data(file: GetFile, width: int = BOARD_WIDTH, height: int = BOARD_HEIGHT) -> Board:
+    """Load robots from file and create a board."""
+    board = Board(width, height)
     for line in file.get_row():
-        position = [int(x) for x in line[0].split('=')[1].split(',')]
-        velocity = [int(x) for x in line[1].split('=')[1].split(',')]
+        position, velocity = parse_robot_data(line)
         robot = Robot(position, velocity)
         board.add_robot(robot)
     return board
 
 
-def part1(board: Board) -> int:
-    board.move_robots(100)
-    return math.prod(board.count_robots())
+def part1(board: Board, steps: int = PART1_STEPS) -> int:
+    """Move robots and return the product of quadrant counts."""
+    board.move_robots(steps)
+    quadrant_counts = board.count_robots_by_quadrant()
+    return math.prod(quadrant_counts)
 
 
 def part2(board: Board) -> int:
+    """Find the first second where all robots are at unique positions."""
     second = 0
     while True:
         second += 1
         board.move_robots(1)
-        if board.no_repeats():
+        if board.has_no_overlaps():
             board.print_board()
             return second
 
 
 def main():
+    """Main entry point."""
     data_file = Path(__file__).parent / 'data/data.txt'
     file = GetFile(str(data_file), delimiter=' ')
+
     board = load_data(file)
     print(part1(board))
+    board = load_data(file)
     print(part2(board))
 
 
