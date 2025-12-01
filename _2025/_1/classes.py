2cb4840dae80ba7169244ce6d5c029acf3f85412
Krzysztof Kokot <krzysiukokot@interia.pl>
2025-12-01 07:15:31 +0100

    Refactoring Task 1 2025 Part 1&2

--- _2025/_1/classes.py
+++ _2025/_1/classes.py
@@ -1,14 +1,24 @@
-from typing import Literal
-
-Rotation = Literal['L', 'R']
-
-def parse_rotation(value: str) -> Rotation:
-    value = value[0]
-    rotation = value[:1]
+def parse_rotation(value: str) -> int:
+    rotation = value[0]
     number = int(value[1:])
     if rotation == 'L':
-        return number * -1
+        return -number
     elif rotation == 'R':
         return number
     else:
         raise ValueError(f"Invalid rotation: {rotation}")
+
+def check_zero_end(act_value: int, rotation: int, password: int) -> tuple[int, int]:
+    act_value = (act_value + rotation) % 100
+    if act_value == 0:
+        password += 1
+    return act_value, password
+
+
+def check_zero(act_value: int, rotation: int, password: int) -> tuple[int, int]:
+    step = -1 if rotation < 0 else 1
+    for _ in range(abs(rotation)):
+        act_value = (act_value + step) % 100
+        if act_value == 0:
+            password += 1
+    return act_value, password
--- _2025/_1/main.py
+++ _2025/_1/main.py
@@ -1,30 +1,16 @@
 from pathlib import Path
 
 from helpers import GetFile
-from _2025._1.classes import parse_rotation
+from _2025._1.classes import parse_rotation, check_zero, check_zero_end
 
 
 def part1(array: list[int]) -> int:
     password = 0
     act_value = 50
     for rotation in array:
-        act_value += rotation
-        act_value %= 100
-        if act_value == 0:
-            password += 1
+        act_value, password = check_zero_end(act_value, rotation, password)
     return password
 
-def check_zero(act_value: int, rotation: int, password: int) -> bool:
-    left = rotation < 0
-    for _ in range(abs(rotation)):
-        act_value += -1 if left else 1
-        if act_value == -1:
-            act_value = 99
-        elif act_value == 100:
-            act_value = 0
-        if act_value == 0:
-            password += 1
-    return act_value, password
 
 def part2(array: list[int]) -> int:
     password = 0
@@ -34,10 +20,7 @@ def part2(array: list[int]) -> int:
     return password
 
 def read_file(file: GetFile) -> list[int]:
-    array = []
-    for row in file.get_row():
-        array.append(parse_rotation(row))
-    return array
+    return [parse_rotation(row[0]) for row in file.get_row()]
 
 def main():
     data_file = Path(__file__).parent / 'data/data.txt'
