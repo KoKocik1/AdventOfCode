b45a4e3daea99bbd034664d12e22f1e5f538ec2b
Krzysztof Kokot <krzysiukokot@interia.pl>
2025-12-07 08:51:20 +0100

    Add Directions to helper init file

--- helpers/__init__.py
+++ helpers/__init__.py
@@ -1,6 +1,6 @@
 
 from .get_file import GetFile
-from .array_helper import Board, Position, Player
+from .array_helper import Board, Position, Player, Directions
 from .timer import time_function, print_timed_result, time_and_print
 
-__all__ = ['GetFile', 'Board', 'Position', 'Player', 'time_function', 'print_timed_result', 'time_and_print']
+__all__ = ['GetFile', 'Board', 'Position', 'Player', 'Directions', 'time_function', 'print_timed_result', 'time_and_print']
